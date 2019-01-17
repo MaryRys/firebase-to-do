5519d32d8b74594a1557670f83c8592f5ae359ce
Mary Rys <rys.mary@gmail.com>
2019-01-16 19:18:56 -0600

    wip

--- src/components/NeedsDoingPage/needsDoing.js
+++ src/components/NeedsDoingPage/needsDoing.js
@@ -4,9 +4,11 @@ import taskData from '../../helpers/data/taskData';
 
 import './needsDoing.scss';
 
-const printSingleTask = (task) => {
-  const cardString = `
-  <div class="container">
+const printTasks = (tasksArray) => {
+  let cardString = '';
+  tasksArray.forEach((task) => {
+    cardString += `
+  <div class="tasksContainer">
   <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">To Do</h5>
@@ -15,17 +17,58 @@ const printSingleTask = (task) => {
       <button class="btn btn-danger delete-btn" data-delete-id=${task.id}>Delete</button>
       <div class="form-check form-check-inline">
       <label class="form-check-label" for="inlineCheckbox1">Is complete?</label>
-      <input class="form-check-input checkIsComplete" type="checkbox" id="${task.id}">
+      <input class="checkIsComplete" type="checkbox" id="${task.id}">
       </div>
     </div>
   </div>
-  </div>
-  `;
-  $('#single-container').append(cardString);
-  if (task.isCompleted) {
-    $('.checkIsComplete').attr('checked', true);
-  }
+  </div>`;
+    $('#single-container').append(cardString);
+  });
 };
+
+// const printCompletedTasks = (completedTasksArray) => {
+//   let cardString = '';
+//   completedTasksArray.forEach((task) => {
+//     cardString += `
+//   <div class="container">
+//   <div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//       <h5 class="card-title">To Do</h5>
+//       <p class="card-text">${task.task}</p>
+//       <button class="btn btn-info edit-btn" data-edit-id=${task.id}>Edit</button>
+//       <button class="btn btn-danger delete-btn" data-delete-id=${task.id}>Delete</button>
+//       <div class="form-check form-check-inline">
+//       <label class="form-check-label" for="inlineCheckbox1">Is complete?</label>
+//       <input class="checkIsComplete" type="checkbox" id="${task.id}">
+//       </div>
+//     </div>
+//   </div>
+//   </div>`;
+//     $('#completed-container').append(cardString);
+//   });
+// };
+
+// const printSingleTask = (task) => {
+//   const cardString = `
+//   <div class="container">
+//   <div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//       <h5 class="card-title">To Do</h5>
+//       <p class="card-text">${task.task}</p>
+//       <button class="btn btn-info edit-btn" data-edit-id=${task.id}>Edit</button>
+//       <button class="btn btn-danger delete-btn" data-delete-id=${task.id}>Delete</button>
+//       <div class="form-check form-check-inline">
+//       <label class="form-check-label" for="inlineCheckbox1">Is complete?</label>
+//       <input class="checkIsComplete" type="checkbox" id="${task.id}">
+//       </div>
+//     </div>
+//   </div>
+//   </div>`;
+//   $('#single-container').append(cardString);
+//   if (task.isCompleted) {
+//     $('.checkIsComplete').attr('checked', true);
+//   }
+// };
 
 const getSingleTask = (e) => {
   const taskId = e.target.dataset.dropdownId;
@@ -38,28 +81,32 @@ const getSingleTask = (e) => {
     });
 };
 
-const buildDropdown = (tasksArray) => {
-  let dropdown = `<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Pick a Task
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
-  if (tasksArray.length) {
-    tasksArray.forEach((task) => {
-      dropdown += `<div class="dropdown-item get-single" data-dropdown-id=${task.id}>${task.task}</div>`;
-    });
-  } else {
-    dropdown += '<div class="dropdown-item">You have no tasks</div>';
-  }
-  dropdown += '<div></div>';
-  $('#dropdown-container').html(dropdown);
-};
+// const buildDropdown = (tasksArray) => {
+//   let dropdown = `<div class="dropdown">
+//   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//     Pick a Task
+//   </button>
+//   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
+//   if (tasksArray.length) {
+//     tasksArray.forEach((task) => {
+//       dropdown += `<div class="dropdown-item get-single" data-dropdown-id=${task.id}>${task.task}</div>`;
+//     });
+//   } else {
+//     dropdown += '<div class="dropdown-item">You have no tasks</div>';
+//   }
+//   dropdown += '<div></div>';
+//   $('#dropdown-container').html(dropdown);
+// };
 
 const tasksPage = () => {
   const uid = authHelpers.getCurrentUid();
   taskData.getAllTasks(uid)
     .then((tasksArray) => {
-      buildDropdown(tasksArray);
+      // const tasksToDo = tasksArray.filter(task => task.isCompleted === false);
+      // const completedTasks = tasksArray.filter(task => task.isComplete === true);
+      printTasks(tasksArray);
+      // printTasks(tasksToDo);
+      // printCompletedTasks(completedTasks);
     })
     .catch((error) => {
       console.error('error in getting tasks', error);
@@ -84,6 +131,7 @@ const updateIsComplete = (e) => {
   const isCompleted = e.target.checked;
   taskData.updatedIsComplete(taskId, isCompleted)
     .then(() => {
+      tasksPage();
     })
     .catch((err) => {
       console.error('error in updating flag', err);
